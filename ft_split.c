e87949b8ca6d60ae2e4f2edd01c83c79f876b910
pmeising <pmeising@student.42wolfsburg.de>
2022-05-13 14:16:56 +0200

    Fixed split, still working on strlcat.

--- ft_split.c
+++ ft_split.c
@@ -6,90 +6,25 @@
 /*   By: pmeising <pmeising@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 14:03:15 by pmeising          #+#    #+#             */
-/*   Updated: 2022/05/11 17:53:23 by pmeising         ###   ########.fr       */
+/*   Updated: 2022/05/13 14:14:33 by pmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #include "libft.h"
-
-// char	*ft_substr(char const *s, unsigned int start, size_t len)
-// {
-// 	size_t	i;
-// 	char	*substr;
-
-// 	i = 0;
-// 	substr = malloc(1 * ((len) + 1));
-// 	while (s[start] != '\0' && i < len)
-// 	{
-// 		substr[i] = s[start];
-// 		i++;
-// 		start++;
-// 	}
-// 	substr[i] = '\0';
-// 	return (substr);
-// }
-
-// char	**ft_count_splits(char const *s, char c)
-// {
-// 	int	i;
-// 	int	j;
-
-// 	i = 0;
-// 	j = 0;
-// 	while (s[j] != '\0')
-// 	{
-// 		if (s[j] == c)
-// 			i++;
-// 		j++;
-// 	}
-// 	return (malloc((i * 3) * 4));
-// }
-
-// char	**ft_split(char const *s, char c)
-// {
-// 	int		i;
-// 	int		j;
-// 	int		len;
-// 	int		k;
-// 	char	**new;
-
-// 	new = ft_count_splits(s, c);
-// 	j = 0;
-// 	i = 0;
-// 	k = 0;
-// 	while (s[j] != '\0')
-// 	{
-// 		if (s[j] == c)
-// 		{
-// 			len = j - i;
-// 			new [k] = ft_substr(s, i, len);
-// 			i = j + 1;
-// 			k++;
-// 		}
-// 		j++;
-// 	}
-// 	len = j - i;
-// 	new[k] = ft_substr(s, i, len);
-// 	return (new);
-// }
 
 #include "libft.h"
 
 int	ft_count_str(char const *s, char c)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
 
 	i = 0;
 	j = 0;
+	if ((s == 0) || (s[0] == '\0'))
+		return (0);
 	while (s[j] != '\0')
 	{
-		if (s[j] == c)
-		{
-			while (s[j] == c)
-				j++;
+		if (s[j] != c && (j == 0 || s[j - 1] == c))
 			i++;
-		}
 		j++;
 	}
 	return (i);
@@ -99,46 +34,27 @@ char	**ft_split(char const *s, char c)
 {
 	int		i;
 	int		j;
-	int		len;
 	int		k;
 	char	**new;
 
-	i = ft_count_str(s, c);
-	if (i < 1)
+	new = malloc(sizeof(char *) * (ft_count_str(s, c) + 1));
+	if (!new || !s)
 		return (NULL);
-	new = malloc((4 * (i * 4)) + 1);
 	j = 0;
-	i = 0;
 	k = 0;
-	while (s[j] != '\0')
+	while (s[j])
 	{
-		if (s[j] == c && s[j - 1] != c)
+		while (s[j] == c)
+			j++;
+		i = j;
+		while (s[j] != c && s[j] != '\0')
+			j++;
+		if (i != j)
 		{
-			len = j - i;
-			new [k] = ft_substr(s, i, len);
-			i = j + 1;
+			new[k] = ft_substr(s, i, (j - i));
 			k++;
 		}
-		if (s[j] == c && s[j - 1] == c)
-			i++;
-		j++;
 	}
-	len = j - i;
-	new[k] = ft_substr(s, i, len);
+	new[k] = 0;
 	return (new);
 }
-
-// int	main(void)
-// {
-// 	char	**array_of_strings;
-// 	int		i;
-
-// // 	i = 0;
-// 	array_of_strings = ft_split("A    Logo     B.", 32);
-// 	while (i < 3)
-// 	{
-// 		printf("%s\n", *(array_of_strings + i));
-// 		i++;
-// 	}
-// 	free(array_of_strings);
-// }
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pmeising <pmeising@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 19:17:41 by pmeising          #+#    #+#             */
-/*   Updated: 2022/05/11 12:47:35 by pmeising         ###   ########.fr       */
+/*   Updated: 2022/05/12 12:58:59 by pmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,3 +32,24 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	dst[i] = '\0';
 	return (ft_strlen(dst) + ft_strlen(&src[j]));
 }
+
+// size_t	ft_strlcat(char *dst, const char *src, size_t size)
+// {
+// 	size_t	i;
+// 	int		j;
+// 	size_t	dst_len;
+
+// 	i = 0;
+// 	j = 0;
+// 	dst_len = ft_strlen(dst);
+// 	if (size <= dst_len)
+// 		return (ft_strlen(src) + size);
+// 	while (src[j] != '\0' && i <= (size - 1))
+// 	{
+// 		dst[dst_len + i] = src[j];
+// 		i++;
+// 		j++;
+// 	}
+// 	dst[i] = '\0';
+// 	return (ft_strlen(dst) + ft_strlen(src));
+// }
