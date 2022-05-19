d9624ac7b0cafe38cea79b7382bc28b7e0f240ad
pmeising <philippmeisinger@googlemail.com>
2022-05-19 20:55:27 +0200

    .

--- ft_substr.c
+++ ft_substr.c
@@ -6,58 +6,17 @@
 /*   By: pmeising <pmeising@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 19:33:56 by pmeising          #+#    #+#             */
-/*   Updated: 2022/05/19 20:53:23 by pmeising         ###   ########.fr       */
+/*   Updated: 2022/05/19 20:55:14 by pmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// char	*ft_substr(char const *s, unsigned int start, size_t len)
-// {
-// 	size_t	i;	
-// 	size_t	s_len;
-// 	char	*substr;
-
-// 	i = 0;
-// 	s_len = ft_strlen(s);
-// 	if (!s)
-// 		return (NULL);
-// 	if (s_len < start)
-// 	{
-// 		substr = malloc(1);
-// 		if (substr == 0)
-// 			return (NULL);
-// 		substr[0] = '\0';
-// 		return (substr);
-// 	}
-// 	if (len > s_len - start)
-// 		len = s_len - start;
-// 	substr = malloc(sizeof(char) * (len + 1));
-// 	if (substr == 0)
-// 		return (NULL);
-// 	while (s[start] != '\0' && i < len && start <= s_len)
-// 	{	
-// 		substr[i] = s[start];
-// 		i++;
-// 		start++;
-// 	}
-// 	substr[i] = '\0';
-// 	return (substr);
-// }
-
-	// if (!s)
-	// 	return (NULL);
-	// else if (!len || (size_t)start >= s_len)
-	// 	return (ft_strdup(""));
-	// else if (len >= s_len)
-	// 	substr = malloc(sizeof(char) * (s_len - start + 1));
-	// else
-
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	total_len;
-	size_t	counter;
+	size_t	i;
 
 	if (!s)
 		return (NULL);
@@ -67,13 +26,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (substr == 0)
 		return (NULL);
 	total_len = start + len;
-	counter = 0;
+	i = 0;
 	while (start < total_len)
 	{
-		substr[counter] = s[start];
+		substr[i] = s[start];
 		start++;
-		counter++;
+		i++;
 	}
-	substr[counter] = 0;
+	substr[i] = 0;
 	return (substr);
 }
