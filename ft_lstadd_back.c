c703d588f6820a9f407aee5756f74706ffc0a2da
pmeising <philippmeisinger@googlemail.com>
2022-05-19 20:16:04 +0200

    .

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: pmeising <pmeising@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 00:14:04 by pmeising          #+#    #+#             */
-/*   Updated: 2022/05/17 20:02:11 by pmeising         ###   ########.fr       */
+/*   Updated: 2022/05/19 20:15:36 by pmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*ptr_to_last;
 	t_list	*ptr_to_head;
 
+	ptr_to_head = *lst;
 	if (*lst == NULL)
 		*lst = new;
-	ptr_to_head = *lst;
-	ptr_to_last = ft_lstlast(ptr_to_head);
-	ptr_to_last->next = new;
-	new->next = NULL;
+	else
+	{
+		while (ptr_to_head->next != NULL)
+			ptr_to_head = ptr_to_head->next;
+		ptr_to_head->next = new;
+	}
 }
